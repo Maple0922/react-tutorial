bed07c9d6f9d759265fc3f62876253102073c4f8
Futo Nakajima <maple0922wh-142857@i.softbank.jp>
2021-01-06 07:56:29 +0900

    Update: code 2021/01/06/07:52

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -3,20 +3,22 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 export default class Layout extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            name: "Tsutomu"
-        };
-    }
+
+     constructor() {
+         super();
+         this.state = {
+             title: "Welcome!"
+         };
+     }
+
+     changeTitle(title) {
+         this.setState({title});
+     }
+
     render() {
-        setTimeout( () => {
-            this.setState({name: "Hello"});
-        }, 1000);
         return (
             <div>
-                {this.state.name}
-                <Header />
+                <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title} />
                 <Footer />
             </div>
         );
